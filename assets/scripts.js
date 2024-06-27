37ffb5ddf57ac64dc157769491af6ea6f7af3301
jeremie <jeremie.herault95@orange.fr>
2024-06-27 11:07:34 +0200

    suppression de bootstrap, jquery

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,19 +1,3 @@
-/* $(document).ready(function () {
-  $(".gallery").mauGallery({
-    columns: {
-      xs: 1,
-      sm: 2,
-      md: 3,
-      lg: 3,
-      xl: 3,
-    },
-    lightBox: true,
-    lightboxId: "myAwesomeLightbox",
-    showTags: true,
-    tagsPosition: "top",
-  });
-});
- */
 class Carousel {
   constructor(element, options = {}) {
     this.element = element;
